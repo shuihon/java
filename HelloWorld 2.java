d1290f115c49013706f4f6bbfd4d88ad1b498469
shuihon <shuhonrin@gmail.com>
2022-10-27 21:24:50 +0900

    第二日の練習　こんにちは、

---
+++ HelloWorld 2.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("こんにちは、初めまして、リンと申します。"
+				);
+		
+		System.out.println("よろしくお願い致します。");
+		System.out.println("今日は令和4年10月二十七日で、");
+		System.out.println( "友達ポンさんと一緒に安安というお店に行て焼く肉を食べました、");
+		System.out.println("僕は290円のビルを飲んで1800円の満足コースを食べました。");
+		System.out.println("合計は2,090円がかかりました。");
+		System.out.println("肉は美味しいですけれども、ポイントを取られないんですから、");
+		System.out.println( "ポンさんと一緒に行ったことから、楽しかったと思いました。");
+        System.out.println("\n");
+		}
+}
