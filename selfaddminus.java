f2a26c12920777d9403082a3de339fdb447a5569
shuihon <shuhonrin@gmail.com>
2022-11-01 22:05:37 +0900

    java 运算符

---
+++ selfaddminus.java
@@ -0,0 +1,14 @@
+public class selfaddminus {
+	public static void main(String[] args) {
+		int a=3;
+		int b=++a;
+		int c=3;
+		int d=--c;
+		
+		System.out.println("self-adding is "+b);
+		System.out.println("self-minusing is"+d);
+		
+				
+				
+	}
+}
