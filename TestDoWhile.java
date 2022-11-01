ab917376cbd3d67e8b874d6488ed9dc01da77098
shuihon <shuhonrin@gmail.com>
2022-11-01 22:34:12 +0900

    three loop structure

---
+++ TestDoWhile.java
@@ -0,0 +1,13 @@
+public class TestDoWhile {
+	public static void main(String [] args) {
+		int x= 10;
+		 
+		do {
+			System.out.println("x equals"+x);
+			x++;
+			System.out.println("\n");
+			
+		}while(x<20);
+	}
+	
+}
---
+++ TestForloop.java
@@ -0,0 +1,11 @@
+public class TestForloop {
+	public static void main(String[] args) {
+		 
+		for(int x= 10; x < 20 ; x=x+1) {
+			System.out.println("x equals"+x);
+			System.out.println("\n");
+			
+		}
+			
+	}
+}
