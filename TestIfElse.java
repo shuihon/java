a34ca482f0a086803a11609cd62fbefde4e1f189
shuihon <shuhonrin@gmail.com>
2022-11-02 22:07:44 +0900

    if else if

---
+++ TestIfElse.java
@@ -0,0 +1,25 @@
+public class TestIfElse {
+	public static void main(String[] args) {
+		int x=30;
+		if (x == 10) {
+			System.out.println("x equals 10");
+		}else if (x==20) {
+			System.out.println("x equals 20");
+		}else if (x==30) {
+			System.out.println("x equals 30");
+		}else {
+			System.out.println("this is else clause");
+		}
+//		if(x<20) {
+//			System.out.println("this is if clause");
+//		}else {
+//			System.out.println("this is else clause");
+//		}
+//		if(x<20) {
+//			System.out.println("this is if clause");
+//		}
+		
+		
+	}
+	
+}
