e24adbc5f6211553b6e7cbceeac7e3918fc25cf7
shuihon <shuhonrin@gmail.com>
2022-11-06 21:20:36 +0900

    JDK，JRE，JVM三者关系

---
+++ hsp,java/javahomework.java
@@ -0,0 +1,11 @@
+public class javahomework {
+	public static void main(String [] args) {
+		System.out.println("JDK,JRE,JVM的关系：\n"
+				+"\nJDK全称是JAVA Development Kit，也就是java工具开发包,"
+				+ "\nJRE是java运行环境，JAVA Runing Environment,"
+				+ "\nJVM是Java虚拟机，JAVA Virtural Mechaine,"
+				+ "\nJava开发工具里面有：Javac，Javadoc，Javap。"
+				+ "\n其中关系是JDK=JRE+JAVA开发工具"
+				+ "\nJRE=JVM+Java核心类库。");
+	}
+}
