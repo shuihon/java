3150c0de81f5055c09a4414cde613350ccca2c87
shuihon <shuhonrin@gmail.com>
2022-11-06 22:17:50 +0900

    dos命令，一些代码规范，绝对路径，相对路径

---
+++ hsp,java/javahwforth.java
@@ -0,0 +1,17 @@
+public class javahwforth {
+	public static void main(String[] args ) {
+		System.out.println("java代码书写规范："
+				+ "\n\t1.养成写注释的习惯"
+				+ "\n\t2.类和方法的注释要用javadoc的方式来写"
+				+ "\n\t3.运算符俩边习惯性留出空格"
+				+ "\n\t4.源文件实用utf-8编码"
+				+ "\n\t5.代码风格行尾风格。"
+				+ "\ndos命令的基本原理："
+				+ "\n\t控制台写出dos命令，dos系统接收，解析，执行三步完成，最后呈现在系统文件中"
+				+ "\n相对路径：\n\t从当前目录文件开始，执行到指定文件的路径"
+				+ "\n绝对路径：\n\t从顶级目录开始，执行移动到指定文件的路径"
+				+ "\n\"..\\\"的意思是往上走一个层级，\"..\\..\\\"则是往上走俩个层级，以此类推"
+				+ "\nmd：\tmake directory"
+				+ "\nrd：\tremake directory");
+	}
+}
