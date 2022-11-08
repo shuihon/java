3efc2a4c6c1c1515ab77536382fa5bebd0f875c3
shuihon <shuhonrin@gmail.com>
2022-11-08 23:08:10 +0900

    理解字符类型的数据底层本质是整数

---
+++ hsp,java/hwchar.java
@@ -0,0 +1,18 @@
+public class hwchar {
+	public static void main (String [] args ) {
+		char a = 92 ;
+		System.out.println(a + "n");
+		char b = 92 ;
+		System.out.println(b + "t");
+		char c = 92  ;
+		System.out.println(c + "r");
+		char c1 = 92 ;
+		System.out.println(c1 + "\\");
+		char d = 49 ;
+		System.out.println(d);
+		char e = 50 ;
+		System.out.println(e);
+		char f = 51 ;
+		System.out.println(f);
+	}
+}
