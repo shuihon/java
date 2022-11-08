bfea0c03496a9c5a033d267c952c6b8d811eb127
shuihon <shuhonrin@gmail.com>
2022-11-08 23:07:34 +0900

    换行输出各类型的数据（章节作业60）

---
+++ hsp,java/hwpingjie.java
@@ -0,0 +1,29 @@
+public class hwpingjie {
+	public static void main (String [] args ) {
+/*		String a = "姓名" ;
+		String b = "年龄";
+		String c = "成绩";
+		String d = "性别";
+		String e = "爱好";
+		String f = "田中";
+		String g = "18";
+		String h = "97";
+		String i = "男";
+		String j = "摄影";
+		System.out.println(a + "\t" + "\t" + b 
+				+ "\t" +  c + "\t" + d + "\t" + e + "\n"  + f 
+				+ "\t" + "\t" + g + "\t" + h + "\t" + i + "\t" + j);
+*/		
+		String name = "jacky";
+		int age = 18 ;
+		double score = 98.99;
+		char gender = '男';
+		String hobby = "摄影";
+		System.out.println("姓名\t性别\t年龄\t成绩\t爱好\n" + 
+		name + "\t" + gender + "\t" + age + "\t" + score + "\t" 
+				+ hobby );
+				
+		
+				
+	}
+}
